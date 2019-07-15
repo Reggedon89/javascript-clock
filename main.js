3d5393bd139e4d919b6459c47d4c0b6ce0f29f61
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-15 14:26:05 -0700

    first and last commit

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(() => {
+  function showTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let session = "AM";
+
+    if (hours === 0) {
+      hours = 12;
+    }
+    if (hours > 12) {
+      hours = hours - 12;
+      session = "PM";
+    }
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let time = hours + ":" + minutes + ":" + seconds + " " + session;
+
+    $("#clock-wrapper").html(time);
+  }
+  setInterval(showTime, 1000);
+  showTime();
+
+  function showHex() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let combine =
+      "#" + hours.toString() + minutes.toString() + seconds.toString();
+    $("#hex-wrapper").html(combine);
+    $("body").css("background", combine);
+  }
+  setInterval(showHex, 1000);
+  showHex();
+});
